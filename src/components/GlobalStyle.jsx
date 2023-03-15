2d188e7ef28b65d4cdb501e1b9daba2c4dd58181
Marcel <mrezende@bcfsolutions.net.br>
2023-03-15 17:58:18 -0300

    feat: styling services section

--- src/components/GlobalStyle.jsx
+++ src/components/GlobalStyle.jsx
@@ -9,7 +9,7 @@ const GlobalStyle = createGlobalStyle`
 }
 body{
     background: #1b1b1b;
-
+    font-family: 'Inter', sans-serif;
 }
 
 button{
@@ -21,6 +21,7 @@ button{
     background: transparent;
     color: white;
     transition: all 0.5s ease;
+    font-family: 'Inter', sans-serif;
     &:hover{
         background-color: #23d997;
         color: white;
@@ -35,6 +36,11 @@ h3{
 }
 h4{
     font-weight: bold;
+    font-size: 2rem;
+    
+}
+a{
+    font-size: 1.1rem;
 }
 span{
     font-weight: bold;
