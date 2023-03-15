2b9283ff81a01a852d7d0e6512e4c14b5fc61820
Marcel <mrezende@bcfsolutions.net.br>
2023-03-15 17:30:35 -0300

    feat: adding styled component to about section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import About from "./pages/About";
 
+// Global Style
+import GlobalStyle from "./components/GlobalStyle";
+
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <About />
     </div>
   );
