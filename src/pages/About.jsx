10f58928bd09ac6d95dd300e55be010253404598
Marcel <mrezende@bcfsolutions.net.br>
2023-03-15 16:45:48 -0300

    feat: add services section to about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,9 +2,15 @@ import React from "react";
 
 // Page components
 import AboutSection from "../components/AboutSection";
+import ServicesSection from "../components/ServicesSection";
 
 const About = () => {
-  return <AboutSection />;
+  return (
+    <>
+      <AboutSection />
+      <ServicesSection />
+    </>
+  );
 };
 
 export default About;
