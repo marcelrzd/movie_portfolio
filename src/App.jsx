955ee0d926d660f4e5d4de289d44f208b529e206
Marcel <mrezende@bcfsolutions.net.br>
2023-03-16 09:41:39 -0300

    feat: adding navbar to the website

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import About from "./pages/About";
 
 // Global Style
 import GlobalStyle from "./components/GlobalStyle";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
+      <Nav />
       <About />
     </div>
   );
