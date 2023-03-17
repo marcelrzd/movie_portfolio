f611c6cf735ba9cc620197d402e863f6ab638fda
Marcel <mrezende@bcfsolutions.net.br>
2023-03-17 11:36:40 -0300

    fix: fixing layout for minor screens feat: adding a active line effect to navlinks feat: adding a layout adaptation to the lines on nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { motion } from "framer-motion";
+import { useLocation } from "react-router-dom";
 
 const Nav = () => {
+  const { pathname } = useLocation();
+
+  const [pageWidth, setPageWidth] = useState(window.innerWidth);
+  useEffect(() => {
+    const handleResize = () => {
+      setPageWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+  const LineClass = pageWidth < 1300 ? "line-mobile" : "";
+
   return (
     <StyledNav>
       <h1>
@@ -15,16 +33,32 @@ const Nav = () => {
           <Link className="link" to="/about">
             1. About Us
           </Link>
+          <Line
+            transition={{ duration: 0.75 }}
+            initial={{ width: "0%" }}
+            animate={{ width: pathname === "/about" ? "50%" : "0%" }}
+          />
         </li>
         <li>
           <Link className="link" to="/work">
             2. Out Work
           </Link>
+          <Line
+            transition={{ duration: 0.75 }}
+            initial={{ width: "0%" }}
+            animate={{ width: pathname === "/work" ? "50%" : "0%" }}
+          />
         </li>
         <li>
           <Link className="link" to="/contact">
-            1. Contact Us
+            3. Contact Us
           </Link>
+          <Line
+            className={LineClass}
+            transition={{ duration: 0.75 }}
+            initial={{ width: "0%" }}
+            animate={{ width: pathname === "/contact" ? "50%" : "0%" }}
+          />
         </li>
       </ul>
     </StyledNav>
@@ -42,6 +76,9 @@ const StyledNav = styled.nav`
   top: 0;
   z-index: 10;
   background: #282828;
+  .line-mobile {
+    width: 100% !important;
+  }
   .link {
     color: white;
     text-decoration: none;
@@ -59,6 +96,32 @@ const StyledNav = styled.nav`
     padding-left: 10rem;
     position: relative;
   }
+  @media (max-width: 1300px) {
+    flex-direction: column;
+    padding: 2rem 1rem;
+    #logo {
+      display: inline-block;
+      margin: 2rem;
+    }
+    ul {
+      padding: 2rem;
+      justify-content: space-around;
+      width: 100%;
+      li {
+        padding: 0;
+      }
+    }
+  }
 `;
-
+const Line = styled(motion.div)`
+  height: 0.3rem;
+  background: #23d997;
+  width: 0%;
+  position: absolute;
+  bottom: -80%;
+  left: 60%;
+  @media (max-width: 1300px) {
+    left: 0%;
+  }
+`;
 export default Nav;
